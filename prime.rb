9f611b9acace056dd14010dec8d51340c1e32f21
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 20:02:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,11 @@
 def prime?(integer)
-  for num in 2..(integer - 1)
-    if (integer % num) == 0 
-      return false 
+  if integer > 0 
+    for num in 2..(integer - 1)
+      if (integer % num) == 0 
+        return false 
+      end 
     end 
-  end 
-  true 
+    true
+  else 
+    false 
   end 
