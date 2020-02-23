f144e03fc5049137b54129e9150e0f8a302bba1d
Tim Donoghue <tmd112593@gmail.com>
2020-02-23 19:53:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   i = 2 
   if integer.negative? || integer == 0 
     return "false"
-    elsif i < integer 
+  elsif i < integer 
       return false if integer % i == 0 && integer < 1 
         i += 1 
    end 
